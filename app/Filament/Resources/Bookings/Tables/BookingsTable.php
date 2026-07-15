1459defe379a01ac72bb26a77291052596730763
Vanny macOS <vannysmilekh@gmail.com>
2026-07-15 22:45:49 +0700

    Refactor import statements in BookingsTable for consistency

--- app/Filament/Resources/Bookings/Tables/BookingsTable.php
+++ app/Filament/Resources/Bookings/Tables/BookingsTable.php
@@ -3,11 +3,10 @@
 namespace App\Filament\Resources\Bookings\Tables;
 
 use App\Actions\CancelBookingAction;
-use Filament\Tables\Actions\BulkActionGroup;
-use Filament\Tables\Actions\DeleteBulkAction;
-use Filament\Tables\Actions\EditAction;
-use Filament\Notifications\Notification;
-use Filament\Tables\Actions\Action;
+use Filament\Actions\Action;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
